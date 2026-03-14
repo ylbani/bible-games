7f06bd40ce4382a71b2b5327e17786a92438fa2a
ylbani <ylbani.hernandez@hotmail.com>
2026-03-14 16:43:57 -0400

    Updates

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'bible-games-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/src/styles/variables.css',
+  '/src/styles/global.css',
+  '/src/styles/screens.css',
+  '/src/styles/games.css',
+  '/src/main.js',
+  '/favicon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        if (response.ok && event.request.method === 'GET') {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      }).catch(() => {
+        if (event.request.destination === 'document') {
+          return caches.match('/index.html');
+        }
+      });
+    })
+  );
+});
